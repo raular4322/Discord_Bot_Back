0c0504c4e98fb5c229f6ea74ff575607d378a0f4
raular4322 <raular98@hotmail.com>
2019-08-08 12:15:18 +0200

    index and app added

---
+++ app.js
@@ -0,0 +1,11 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const userRoutes = require('./routes/userRoutes');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use('/user', userRoutes);
+
+module.exports = app;
