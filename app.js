c5e26eaea2ee8c2d51cec511d3975e415618c187
raular4322 <raular98@hotmail.com>
2019-08-11 21:40:42 +0200

    admin route addded to app.js

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const userRoutes = require('./routes/userRoutes');
+const adminRoutes = require('./routes/adminRoutes');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use('/user', userRoutes);
+app.use('/admin', adminRoutes);
 
 module.exports = app;
